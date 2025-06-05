d257c3501e9996a851158479f0d8db7596496f25
Sota-Akahane <sota258@docomo.ne.jp>
2025-06-05 11:34:57 +0900

    車を表すドメインクラスを作りました。

---
+++ src/main/java/com/example/domain/Car.java
@@ -0,0 +1,16 @@
+package com.example.domain;
+
+/**
+ * 車を表すぜ.
+ */
+public class Car {
+    private int speed;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
