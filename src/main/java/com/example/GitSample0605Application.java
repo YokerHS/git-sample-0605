72712810ac2bcd409d0e8abd5f9da65b832a011d
YokerHS <hiimyoker@gmail.com>
2025-06-05 11:34:49 +0900

    add car

--- src/main/java/com/example/GitSample0605Application.java
+++ src/main/java/com/example/GitSample0605Application.java
@@ -1,4 +1,4 @@
-package com.example.git_sample_0605;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
